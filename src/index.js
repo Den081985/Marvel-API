193f1334ed9088574d41b12b1f86f81c4cf43b78
Den081985 <pishnenkod@gmail.com>
2021-12-18 22:48:07 +0300

    add imports and async function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("hello3");
+/*Импортируем аксиос,создаем ключ для апи и асинхронную самовызывающуюся
+функцию с методом класса getDataApi.getData
+ */
+
+// const API_URL = "https://gateway.marvel.com/v1/public/comics";
+
+// const API_URL = "https://gateway.marvel.com/v1/public/characters";
+
+import { API_KEY, API_URL, URL_COMICS } from "../constants/api";
+
+import { getDataApi } from "./utils/getDataApi";
+
+// Асинхронная самовызывающаяся функция
+
+(async () => {
+  const data = await getDataApi.getData(API_URL + URL_COMICS);
+  console.log(data);
+})();
