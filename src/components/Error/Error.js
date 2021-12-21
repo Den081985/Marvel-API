47692627775bb91f8f9ee45f2b11ae81b0b651d7
Den081985 <pishnenkod@gmail.com>
2021-12-21 13:39:06 +0300

    add Error.js, Error.css, Index.js

---
+++ src/components/Error/Error.js
@@ -0,0 +1,23 @@
+// Создаем класс для отображения ошибки
+
+import { ROOT_INDEX } from "../../../constants/root";
+
+import classes from "./Error.css";
+
+class Error {
+  render() {
+    const htmlWrapper = `
+        <div class = "${classes.error__container}">
+            <span>
+                <p class = "${classes.error__alert}">Произошла ошибка</p>
+                <p class = "${classes.error__alert}">Попробуйте перезагрузить страницу</p>
+            </span>
+        </div>
+      
+      `;
+
+    ROOT_INDEX.innerHTML = htmlWrapper;
+  }
+}
+
+export default new Error();
