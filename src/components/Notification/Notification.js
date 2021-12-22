b856d9b3db1b58c90b383e632b6e49be3c0b7a31
Den081985 <pishnenkod@gmail.com>
2021-12-22 22:32:14 +0300

    add Notification class

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,29 @@
+//Создаем класс для отображения элемента,информирующего о отсутствии персонажа комикса
+
+import { ROOT_MODAL } from "../../../constants/root";
+
+import classes from "./Notification.css";
+
+import imgCloseBlack from "./Img/icons8-удалить.svg";
+
+class Notification {
+  render() {
+    const htmlWrapper = `
+    
+        <div class = "${classes.notification__container}">
+            <span class = "${classes.notification__text}">No contents</span>
+            <button class = "btn btn-contain ${classes.notification__close}"
+            onclick = "modal.innerHTML = ''"
+            style = "background-image: url(${imgCloseBlack})"
+            ></button>
+
+        
+        </div>
+    
+    `;
+
+    ROOT_MODAL.innerHTML = htmlWrapper;
+  }
+}
+
+export default new Notification();
